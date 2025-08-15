d50f0a704c687a335caf0cac9dac12abff35c4cb
phosa0 <rohailhabib0@gmail.com>
2025-08-15 18:55:31 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define UNICODE
+#define _UNICODE
+
 #include <Windows.h>
 #include <CommCtrl.h>
 #include <string>
@@ -7,20 +10,17 @@
 
 #pragma comment(lib, "comctl32.lib")
 
-// Global handles for UI controls
 HWND g_h_wnd;
 HWND g_h_process_list;
 HWND g_h_dll_path_edit;
 
-// Function prototypes
 LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param);
 void create_controls(HWND hwnd);
 void populate_process_list();
 void handle_inject();
 void handle_browse();
 
-int WINAPI WinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance, LPSTR lp_cmd_line, int n_cmd_show) {
-    // Register the window class
+int WINAPI wWinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance, PWSTR lp_cmd_line, int n_cmd_show) {
     const wchar_t CLASS_NAME[] = L"DllInjectorWindowClass";
     WNDCLASSW wc = {};
     wc.lpfnWndProc = wnd_proc;
@@ -30,7 +30,6 @@ int WINAPI WinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance, LPSTR lp_cmd
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     RegisterClassW(&wc);
 
-    // Create the window
     g_h_wnd = CreateWindowExW(
         0, CLASS_NAME, L"Simple DLL Injector",
         WS_OVERLAPPEDWINDOW,
@@ -38,14 +37,11 @@ int WINAPI WinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance, LPSTR lp_cmd
         NULL, NULL, h_instance, NULL
     );
 
-    if (g_h_wnd == NULL) {
-        return 0;
-    }
+    if (!g_h_wnd) return 0;
 
     ShowWindow(g_h_wnd, n_cmd_show);
     UpdateWindow(g_h_wnd);
 
-    // Message loop
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
@@ -63,15 +59,9 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) {
             break;
         case WM_COMMAND:
             switch (LOWORD(w_param)) {
-                case IDC_REFRESH_BUTTON:
-                    populate_process_list();
-                    break;
-                case IDC_INJECT_BUTTON:
-                    handle_inject();
-                    break;
-                case IDC_BROWSE_BUTTON:
-                    handle_browse();
-                    break;
+                case IDC_REFRESH_BUTTON: populate_process_list(); break;
+                case IDC_INJECT_BUTTON: handle_inject(); break;
+                case IDC_BROWSE_BUTTON: handle_browse(); break;
             }
             break;
         case WM_DESTROY:
@@ -84,35 +74,26 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) {
 }
 
 void create_controls(HWND hwnd) {
-    // Create Process List (ListView)
     g_h_process_list = CreateWindowExW(WS_EX_CLIENTEDGE, WC_LISTVIEWW, L"",
         WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL,
         10, 10, 350, 300, hwnd, (HMENU)IDC_PROCESS_LIST, NULL, NULL);
 
-    // Add columns to the ListView
-    LVCOLUMNW lvc;
+    LVCOLUMNW lvc = {};
     lvc.mask = LVCF_TEXT | LVCF_WIDTH | LVCF_SUBITEM;
-    lvc.cx = 250;
-    lvc.pszText = (LPWSTR)L"Process Name";
+    lvc.cx = 250; lvc.pszText = (LPWSTR)L"Process Name";
     ListView_InsertColumn(g_h_process_list, 0, &lvc);
-
-    lvc.cx = 100;
-    lvc.pszText = (LPWSTR)L"PID";
+    lvc.cx = 100; lvc.pszText = (LPWSTR)L"PID";
     ListView_InsertColumn(g_h_process_list, 1, &lvc);
 
-    // Create Refresh Button
     CreateWindowW(L"BUTTON", L"Refresh", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
         370, 10, 100, 30, hwnd, (HMENU)IDC_REFRESH_BUTTON, NULL, NULL);
 
-    // Create DLL Path Edit Control
     g_h_dll_path_edit = CreateWindowW(L"EDIT", L"", WS_BORDER | WS_CHILD | WS_VISIBLE,
         10, 320, 260, 25, hwnd, (HMENU)IDC_DLL_PATH_EDIT, NULL, NULL);
 
-    // Create Browse Button
     CreateWindowW(L"BUTTON", L"Browse...", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
         280, 320, 80, 25, hwnd, (HMENU)IDC_BROWSE_BUTTON, NULL, NULL);
 
-    // Create Inject Button
     CreateWindowW(L"BUTTON", L"Inject", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
         370, 50, 100, 30, hwnd, (HMENU)IDC_INJECT_BUTTON, NULL, NULL);
 }
@@ -131,7 +112,7 @@ void populate_process_list() {
 
         wchar_t pid_str[20];
         swprintf(pid_str, 20, L"%lu", processes[i].pid);
-        ListView_SetItemText(g_h_process_list, i, 1, pid_str);
+        ListView_SetItemTextW(g_h_process_list, i, 1, pid_str);
     }
 }
 
